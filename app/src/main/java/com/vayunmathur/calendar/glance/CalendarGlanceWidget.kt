e54a813bcc41d38918c947fbb7ce0c9ee705a3ae
Your Name <you@example.com>
2025-12-30 15:19:19 +0800

    fix glance widget

--- app/src/main/java/com/vayunmathur/calendar/glance/CalendarGlanceWidget.kt
+++ app/src/main/java/com/vayunmathur/calendar/glance/CalendarGlanceWidget.kt
@@ -2,27 +2,19 @@ package com.vayunmathur.calendar.glance
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CalendarToday
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.ImageProvider
+import androidx.glance.action.actionParametersOf
+import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
+import androidx.glance.action.toParametersKey
 import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.components.Scaffold
 import androidx.glance.appwidget.components.TitleBar
 import androidx.glance.appwidget.cornerRadius
@@ -33,43 +25,33 @@ import androidx.glance.background
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
-import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxHeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
-import androidx.glance.layout.height
 import androidx.glance.layout.padding
 import androidx.glance.layout.width
-import androidx.glance.layout.wrapContentHeight
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextDefaults.defaultTextStyle
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vayunmathur.calendar.Calendar
 import com.vayunmathur.calendar.Event
 import com.vayunmathur.calendar.MainActivity
 import com.vayunmathur.calendar.R
-import com.vayunmathur.calendar.ui.PositionedEvent
 import com.vayunmathur.calendar.ui.computePositionedEventsForDay
 import com.vayunmathur.calendar.ui.dateFormat
 import com.vayunmathur.calendar.ui.dateRangeString
-import com.vayunmathur.calendar.ui.theme.CalendarTheme
 import com.vayunmathur.calendar.ui.theme.CalendarThemeGlance
-import kotlinx.coroutines.launch
 import kotlinx.datetime.DatePeriod
-import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.daysUntil
 import kotlinx.datetime.format
 import kotlinx.datetime.format.DayOfWeekNames
 import kotlinx.datetime.format.MonthNames
 import kotlinx.datetime.format.Padding
 import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
-import kotlin.collections.listOf
 import kotlin.time.Clock
 
 class CalendarGlanceWidget : GlanceAppWidget() {
@@ -93,14 +75,14 @@ class CalendarGlanceWidget : GlanceAppWidget() {
 
         provideContent {
             CalendarThemeGlance(context) {
-                Content(context, positionedEvents, events.filter { it.spanDays.last() > today && it.spanDays.first() < nextMonth }, calendars)
+                Content(positionedEvents, calendars)
             }
         }
     }
 }
 @SuppressLint("RestrictedApi")
 @Composable
-fun Content(context: Context, positionedEvents: Map<LocalDate, List<Event>>, events: List<Event>, calendars: Map<Long, Calendar>) {
+fun Content(positionedEvents: Map<LocalDate, List<Event>>, calendars: Map<Long, Calendar>) {
     val dateFormatS = LocalDate.Format {
         dayOfWeek(DayOfWeekNames.ENGLISH_ABBREVIATED)
         chars(", ")
@@ -116,11 +98,7 @@ fun Content(context: Context, positionedEvents: Map<LocalDate, List<Event>>, eve
     val days = today..<nextMonth
 
     Scaffold(titleBar = {
-        TitleBar(ImageProvider(R.drawable.calendar_today_24px), today.format(dateFormatS), modifier = GlanceModifier.clickable{
-            val intent = Intent(context, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            context.startActivity(intent)
-        })
+        TitleBar(ImageProvider(R.drawable.calendar_today_24px), today.format(dateFormatS), modifier = GlanceModifier.clickable(actionStartActivity<MainActivity>()))
     }) {
         LazyColumn(modifier = GlanceModifier.fillMaxSize()) {
             for(day in days) {
@@ -131,12 +109,7 @@ fun Content(context: Context, positionedEvents: Map<LocalDate, List<Event>>, eve
                 }
                 items(positionedEvents[day]!!) { orEvent ->
                     Row(modifier = GlanceModifier.fillMaxWidth().padding(vertical = 6.dp)) {
-                        Row(GlanceModifier.background(GlanceTheme.colors.primaryContainer).cornerRadius(6.dp).clickable {
-                            val intent = Intent(context, MainActivity::class.java)
-                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                            intent.putExtra("id", orEvent.id!!)
-                            context.startActivity(intent)
-                        }) {
+                        Row(GlanceModifier.background(GlanceTheme.colors.primaryContainer).cornerRadius(6.dp).clickable (actionStartActivity<MainActivity>(actionParametersOf(longPreferencesKey("id").toParametersKey() to orEvent.id!!)))) {
                             Box(GlanceModifier.background(ColorProvider(Color(orEvent.color ?: calendars[orEvent.calendarID]!!.color))).width(8.dp).fillMaxHeight()) {}
                             Column(GlanceModifier.padding(4.dp).fillMaxWidth()) {
                                 Text(
@@ -161,4 +134,5 @@ fun Content(context: Context, positionedEvents: Map<LocalDate, List<Event>>, eve
         }
     }
 }
+
 
