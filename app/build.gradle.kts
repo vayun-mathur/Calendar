fb2ac0d376e2c2f6f85996c5d55e55822ad81d05
Your Name <you@example.com>
2025-12-31 19:50:18 +0800

    updated build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,26 +13,17 @@ android {
     defaultConfig {
         applicationId = "com.vayunmathur.calendar"
         minSdk = 31
-        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+
     buildFeatures {
         compose = true
     }
